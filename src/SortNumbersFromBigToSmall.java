317b71cadfad3c505405f8d8afdf023cad81f154
mueyyed <moayyadarz92@gmail.com>
2021-07-05 10:55:09 +0300

    code to sort numbers descendingly

---
+++ src/SortNumbersFromBigToSmall.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortNumbersFromBigToSmall {
+    public static void main(String[] args) {
+
+        int []array=new int[3];
+        Scanner scanner=new Scanner(System.in);
+        for(int i=0; i<3; i++)
+        {
+            System.out.println("enter the number " + i +" ");
+            array[i]=scanner.nextInt();
+        }
+        System.out.println("number are sorted from big to small as follows ");
+        Arrays.sort(array);
+        for(int i =array.length -1; i>=0;i--)
+        {
+            System.out.print(array[i] + " ");
+        }
+
+        //     Vector<Integer> array = new Vector<Integer>(3);
+        // we can use another sturctures to get these elements sorted
+    }
+}
