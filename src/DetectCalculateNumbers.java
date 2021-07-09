f7b7e5f9e4d86d33deca76a642995b80b53dd33c
mueyyed <moayyadarz92@gmail.com>
2021-07-09 07:12:17 +0300

    this code if to give grading for student in terms of letters ( AA===FF)

---
+++ src/DetectCalculateNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DetectCalculateNumbers {
+    public static void main(String[] args) {
+        int number=0;
+        Scanner scanner=new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Enter number");
+
+        }
+    }
+}
---
+++ src/Puanlandirma_Not.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Puanlandirma_Not {
+    public static void main(String[] args) {
+
+        float MidTerm,finalExam;
+        float average ;
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter your Midterm grade");
+        MidTerm=scanner.nextFloat();
+        System.out.println("Enter your Final grade");
+        finalExam=scanner.nextFloat();
+
+        average=MidTerm*(0.5f) + finalExam*(0.7f);
+
+       if(average>0 && average<20)
+           System.out.println("FF");
+       else if(average>=20 && average<50)
+           System.out.println("CB");
+       else if(average>=50 && average<70)
+           System.out.println("BB");
+       else if(average>=70 && average<=100)
+           System.out.println("AA");
+    }
+}
